daf2979dbf690558350ebf754ab47f0c00afc0a5
Ateros <example@example.com>
2025-06-27 12:42:53 +0300

    #4

--- scripts/player-controller.gd
+++ scripts/player-controller.gd
@@ -1,21 +1,24 @@
 extends CharacterBody3D
 
-#@export var speed : float = 5.0
 const SPEED : float = 3.0
 const JUMP_HEIGHT : float = 5.0
 
-#@onready var ground : Node3D = $'../Environment/Ground'
-#@onready var ground : CSGBox3D = $"../Environmet/Ground"
 @onready var camera_pivot: Node3D = $CameraPivot
-@onready var player_mesh: MeshInstance3D = $PlayerMesh
+@onready var player_mesh: Node3D = $PlayerMesh
+@onready var animation_player: AnimationPlayer = $PlayerMesh/AnimationPlayer
+
+enum AnimationState {IDLE, WALK}
+var animation_state : AnimationState = AnimationState.IDLE
 
 func _process(delta : float) -> void:
-	#position.x -= 0.1 * delta
-	#rotation.y -= 0.1
-	#scale.x += 0.1
-	#transform.origin == position
-	#transform.basis == basis
-	pass
+	if not is_on_floor():
+		animation_player.play('jump') #.pause('jump') .stop()
+	else:
+		match(animation_state):
+			AnimationState.IDLE:
+				animation_player.play('idle')
+			AnimationState.WALK:
+				animation_player.play('walk')
 	
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -23,13 +26,6 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed('jump') and is_on_floor():
 		velocity.y = JUMP_HEIGHT
-	
-	#if Input.is_action_pressed('left'):
-		#velocity.x = -SPEED
-	#elif Input.is_action_pressed('right'):
-		#velocity.x = SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0.0, SPEED)
 		
 	var input_dir := Input.get_vector('left', 'right', 'up', 'down')
 	var direction := (camera_pivot.basis * Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
@@ -37,16 +33,10 @@ func _physics_process(delta: float) -> void:
 		player_mesh.basis = camera_pivot.basis
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		animation_state = AnimationState.WALK
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 		velocity.z = move_toward(velocity.z, 0.0, SPEED)
+		animation_state = AnimationState.IDLE
 	
-	#velocity.x -= 0.1
 	move_and_slide()
-	
-	
-	
-	
-	
-	
-	
